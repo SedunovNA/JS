5f626cea922fd19e8d31ad22288ebb2114dc4301
SedunovNA <90080517+SedunovNA@users.noreply.github.com>
2022-12-05 22:55:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-"import React, {useState} from 'react'; 
-
-function App() {
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+function Welcome(props) {
     // let a = 1;
     // let b = 9;
     // let c = a + b;
     // b +=a + b;
     // console.log(c, b);
     const [value, setValue] = useState();
-
     return <>
-        <p>
-            Привет, {value ? value.target.value : ''}
-        </p>
-
-        <input type={'text'} onChange={setValue}/>
+        
+           <br/> {props.name} {value ? value.target.value : ''}
+        
+        <input type={'text'} onChange={setValue} />
     </>;
 }
+function App() {
+    return (
+        <div>
+            <Welcome name="FIO:" /> <br/>
+            <Welcome name="Nomer:" /><br />
+            <Welcome name="Email:" /><br />
+            <Welcome name="data rogdeniya:" /><br />
+            <Welcome name="biografia:" /><br />
+        </div>
+            
+       
+    );
 
-export default App;"
+}
+export default Welcome; 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
